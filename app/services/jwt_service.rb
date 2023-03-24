3a69caec93d7592ec2e2166aa60985eef4f08786
Paul Anthony McGowan <xhostcom@aol.com>
2023-03-24 08:42:19 +0000

    Algorithm

--- app/services/jwt_service.rb
+++ app/services/jwt_service.rb
@@ -1,17 +1,17 @@
 
 class JwtService
+    ALGO = 'HS256'
     def self.encode(payload:, exp: 6.hours.from_now)
         payload[:exp] = 6.hours.from_now.to_i
         JWT.encode(payload, self.secret)
     end
 
     def self.decode(token:)
-        decoded = JWT.decode(token, self.secret).first
+        decoded = JWT.decode(token, self.secret, true, { algorithm: ALGO }).first
         HashWithIndifferentAccess.new decoded
     end
 
     def self.secret
         Rails.application.credentials.devise[:jwt_secret_key]
-    end
+    end 
 end
-
