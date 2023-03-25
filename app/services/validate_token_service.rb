9fc862c777a513504638154db59fbb13975b669b
Paul Anthony McGowan <xhostcom@aol.com>
2023-03-25 11:17:32 +0000

    validate token service OK

--- app/services/validate_token_service.rb
+++ app/services/validate_token_service.rb
@@ -8,29 +8,15 @@ class ValidateTokenService
         return false if @token.nil?
         decoded_token = JwtService.decode(token: @token)
         return false if decoded_token.empty?
-        puts "decoded_token: #{decoded_token}"
-    
-       # decoded_token = JwtService.decode(token: @token)
-       #  return false if decoded_token.empty?
-
-      #  user_id = decoded_token[0]['user_id']
-
-      #  puts "user_id: #{user_id}"
-
-       # return false if decoded_token.empty?
-    
-      #  user_id = decoded_token[0]['user_id']
-       # return false unless user_id
-    
-       # user = User.find_by(id: user_id)
-       # return false unless user
-    
-       # true
-      #  rescue JWT::ExpiredSignature
-     #   false
-      #  rescue JWT::DecodeError
-      #  false
-     #   end
+        user_id = decoded_token['user_id']
+        return false unless user_id
+        user = User.find_sole_by(id: user_id)
+        return false unless user 
+        true
+        rescue JWT::ExpiredSignature
+        false
+        rescue JWT::DecodeError
+        false
     end
 
 end 
